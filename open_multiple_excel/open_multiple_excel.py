68a3d5416a9cf70fd535f54538166d7f411632a2
jingzact <37523337+jingzact@users.noreply.github.com>
2020-03-26 23:43:04 -0400

    Add files via upload

---
+++ open_multiple_excel/open_multiple_excel.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+
+cwd = os.path.abspath('')
+files = os.listdir(cwd)
+
+df = pd.DataFrame()
+
+for file in files:
+    if file.endswith('.xlsx'):
+        df = df.append(pd.read_excel(file), ignore_index=True)
+
+df.to_excel('total_sales.xlsx')
+        
